8a68882e0212b587cbd1e55a93667e53b9428a1a
R3dgrave <r3dgrave98@gmail.com>
2024-11-16 03:29:01 -0300

    .

--- src/app/core/token-http-interceptor.ts
+++ src/app/core/token-http-interceptor.ts
@@ -1,13 +1,15 @@
 import { HttpInterceptorFn } from "@angular/common/http";
 
-export const tokenHttpInterceptor:HttpInterceptorFn = (req, next) =>{
-    const token = localStorage.getItem("token");
-    if(token){
-        req = req.clone({
-            setHeaders: {
-                Authorization: token,
-            }
-        })
-    }
-    return next(req);
-}
+export const tokenHttpInterceptor: HttpInterceptorFn = (req, next) => {
+	const token = localStorage.getItem("token");
+
+	if (token) {
+		req = req.clone({
+			setHeaders: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+	}
+
+	return next(req);
+};
